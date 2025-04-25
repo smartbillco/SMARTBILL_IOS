95b06efef7d12498faef2d60e611e8d24f0751ff
AnnManjarres <ann.manjarres91@gmail.com>
2025-04-25 14:56:24 -0500

    ios2

--- lib/screens/QRcode/qrcode_link_screen.dart
+++ lib/screens/QRcode/qrcode_link_screen.dart
@@ -45,64 +45,17 @@ class _QrcodeLinkScreenState extends State<QrcodeLinkScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 35),
         child: InAppWebView(
-          initialSettings: InAppWebViewSettings(
-            javaScriptEnabled: true,
-            useOnDownloadStart: true,
-            allowFileAccess: true,
-            allowContentAccess: true,
-            useHybridComposition: true,
+            initialUrlRequest: URLRequest(
+              url: WebUri(widget.uri!),
+            ),
+            onWebViewCreated: (controller) {
+              webViewController = controller;
+              print("WebView Created");
+            },
+            onLoadStop: (controller, url) async {
+              print("Loaded: $url");
+            },
           ),
-          initialUrlRequest: URLRequest(url: WebUri(widget.uri!)),
-          onWebViewCreated: (controller) {
-            webViewController = controller;
-          },
-          onLoadStart: (controller, url) {
-            print(url.toString());
-            if(url.toString().contains('ShowDocument')) {
-              cloudflarePassed = true;
-            } else {
-              cloudflarePassed = false;
-            }
-            print("On load: $originalUrl");
-            originalUrl ??= url.toString();
-            
-            print("On load: $url");
-            print("On load: $originalUrl");
-          
-            
-          },
-          shouldOverrideUrlLoading: (controller, navigationAction) async {
-            final url = navigationAction.request.url;
-
-            final dir = Platform.isAndroid
-              ? await getExternalStorageDirectory()
-              : await getApplicationDocumentsDirectory();
-
-
-          },
-          onUpdateVisitedHistory: (controller, url, isReload) {
-            // Option 1: URL check
-            print("onUpdate 1: $url");
-            print("onUpdate 2: $cloudflarePassed");
-            print("onUpdate 3: $originalUrl");
-            print("onUpdate 4: $url");
-
-            if(Platform.isAndroid) {
-
-              if(originalUrl != null && originalUrl != url.toString() && !hasNavigated && cloudflarePassed) {
-                hasNavigated = true;
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ConfirmDownloadScreen(url: url.toString())));
-              } else {
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Por favor espera a que cloudflare te autentique")));
-
-              }
-              
-            }
-            print("Triggered updated");
-            
-          },
-          
-        ),
       ),
     );
   }
